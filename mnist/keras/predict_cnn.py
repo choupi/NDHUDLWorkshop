86b17faf85af60fa3a9f79970accaaec497e3878
choupi <chromosome460@gmail.com>
2016-06-21 17:11:08 +0800

    keras MNIST example with scikit-learn metric

---
+++ mnist/keras/predict_cnn.py
@@ -0,0 +1,62 @@
+'''Trains a simple convnet on the MNIST dataset.
+
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import *
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from sklearn.metrics import *
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+nb_filters = 32
+# size of pooling area for max pooling
+nb_pool = 2
+# convolution kernel size
+nb_conv = 3
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+#X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+#X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+#X_train /= 255
+X_test /= 255
+#print('X_train shape:', X_train.shape)
+#print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+#Y_train = np_utils.to_categorical(y_train, nb_classes)
+#Y_test = np_utils.to_categorical(y_test, nb_classes)
+Y_test = [int(y) for y in y_test]
+
+model = model_from_json(open('mnist_model.json').read())
+model.load_weights('mnist_weights.h5')
+
+y_predict = model.predict_proba(X_test, verbose=0)
+print(log_loss(Y_test, y_predict))
+#y_predict = model.predict(X_test, verbose=0)
+#print(y_predict)
+predict=[]
+for yy in y_predict: predict.append(np.argmax(yy))
+#print(predict)
+print(f1_score(Y_test, predict))
+print(classification_report(Y_test, predict))
+print(confusion_matrix(Y_test, predict))
